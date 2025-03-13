0bf1122a252c0b2ab605320802f2c2a50e5a4f76
Swayamjeet <swayam.hp001@gmail.com>
2025-03-13 23:51:46 +0530

    Added player ship and its rotation and movements

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
         screen.fill(0)
+        for elem in drawable:
+            elem.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
